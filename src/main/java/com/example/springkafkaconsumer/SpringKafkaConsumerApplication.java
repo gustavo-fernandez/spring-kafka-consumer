fae8fc5fe32c34a2119fcc74715ea5f7fcba6c56
Gustavo Fernandez <gustavocfernandez@bcp.com.pe>
2022-01-06 20:52:57 -0500

    reactive consumer

--- src/main/java/com/example/springkafkaconsumer/SpringKafkaConsumerApplication.java
+++ src/main/java/com/example/springkafkaconsumer/SpringKafkaConsumerApplication.java
@@ -1,9 +1,19 @@
 package com.example.springkafkaconsumer;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.KafkaListener;
+import reactor.core.publisher.Flux;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+import reactor.kafka.receiver.ReceiverRecord;
 
 @SpringBootApplication
 @Slf4j
@@ -13,9 +23,26 @@ public class SpringKafkaConsumerApplication {
 		SpringApplication.run(SpringKafkaConsumerApplication.class, args);
 	}
 
-	@KafkaListener(topics = "spring-topic")
-	public void listenToMessages(String data) {
-		log.info("Mensaje recibido: '{}'", data);
+	@PostConstruct
+	public void postConstruct() {
+		Map<String, Object> config = new HashMap<>();
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		config.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group");
+		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+		var receiverOptions = ReceiverOptions.<String, String>create(config)
+			.subscription(List.of("spring-topic"));
+
+		var kafkaReceiver = KafkaReceiver.create(receiverOptions);
+
+		Flux<ReceiverRecord<String, String>> kafkaFlux = kafkaReceiver.receive()
+			.doOnSubscribe(s -> log.info("-- Client subscribed"));
+
+		kafkaFlux.subscribe(response -> {
+			log.info("Mensaje recibido: \nkey: {}\nvalue: {}\nheaders: {}",
+				response.key(), response.value(), response.headers());
+		});
 	}
 
 }
