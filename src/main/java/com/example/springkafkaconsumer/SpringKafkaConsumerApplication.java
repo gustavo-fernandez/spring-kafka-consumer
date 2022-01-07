903ff62dd2958aa5ac4e4aa1934be3e2738c69aa
Gustavo Fernandez <gustavocfernandez@bcp.com.pe>
2022-01-06 21:11:55 -0500

    JSON

--- src/main/java/com/example/springkafkaconsumer/SpringKafkaConsumerApplication.java
+++ src/main/java/com/example/springkafkaconsumer/SpringKafkaConsumerApplication.java
@@ -1,5 +1,6 @@
 package com.example.springkafkaconsumer;
 
+import com.example.springkafkaproducer.Person;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,9 +8,9 @@ import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import reactor.core.publisher.Flux;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
@@ -29,14 +30,16 @@ public class SpringKafkaConsumerApplication {
 		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		config.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group");
 		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+		config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+		config.put(JsonDeserializer.VALUE_DEFAULT_TYPE, Person.class);
 
-		var receiverOptions = ReceiverOptions.<String, String>create(config)
-			.subscription(List.of("spring-topic"));
+		var receiverOptions = ReceiverOptions.<String, Person>create(config)
+			.subscription(List.of("spring-2-topic"));
 
 		var kafkaReceiver = KafkaReceiver.create(receiverOptions);
 
-		Flux<ReceiverRecord<String, String>> kafkaFlux = kafkaReceiver.receive()
+		Flux<ReceiverRecord<String, Person>> kafkaFlux = kafkaReceiver.receive()
 			.doOnSubscribe(s -> log.info("-- Client subscribed"));
 
 		kafkaFlux.subscribe(response -> {
